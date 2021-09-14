5cb153bd4d70278ce78b1eea075fdad16ef6b925
Yuu <anpanmamZ3624@gmail.com>
2021-09-14 20:36:56 +0900

    scss変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,15 +6,24 @@
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
+import ProfileImage from "src/profile_image_upload";
+import Swipe from "src/swipe";
+
 import "channels";
 import "bootstrap";
+import "hammerjs";
 import "../stylesheets/application";
+import "@fortawesome/fontawesome-free/js/all";
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+// ProfileImage.start();
+// Swipe.start();
 
 // require("@rails/ujs").start();
 // require("turbolinks").start();
 // require("@rails/activestorage").start();
 // require("channels");
+// require("src/profile_image_upload");
+// require("src/swipe")
